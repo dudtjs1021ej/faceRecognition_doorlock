4a7a4bae1f19c834bf5a75e25d46934ca7f378fd
yoooungsun <dudtjs1021@hansung.ac.kr>
2021-02-20 22:00:01 +0900

    도어락 제어 클릭 이벤트 추가

--- app/src/main/java/com/example/facerecognitionlock/Fragment_Lock.java
+++ app/src/main/java/com/example/facerecognitionlock/Fragment_Lock.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class Fragment_Lock extends Fragment {
+
+    ImageButton door_btn;
+    String door="close";
 
     public Fragment_Lock() {
         // Required empty public constructor
@@ -18,7 +22,25 @@ public class Fragment_Lock extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_lock, container, false);
+
+        door_btn=(ImageButton)view.findViewById(R.id.imageButton);
+        door_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(door.equals("open")) {
+                    door_btn.setImageDrawable(getActivity().getDrawable(R.drawable.door_close_remove));
+                    door="close";
+                }
+                else if(door.equals("close")) {
+                    door_btn.setImageDrawable(getActivity().getDrawable(R.drawable.door_open_remove));
+                    door="open";
+                }
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lock, container, false);
+        return view;
+
+
     }
 }
