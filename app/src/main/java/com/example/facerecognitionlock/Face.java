95e99b38c33447c184f596464357ca04ee8230c3
dmswn0210 <dmswn021030@gmail.com>
2021-02-24 15:08:57 +0900

    .

---
+++ app/src/main/java/com/example/facerecognitionlock/Face.java
@@ -0,0 +1,261 @@
+package com.example.facerecognitionlock;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Face extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    String mCurrentPhotoPath;
+    final static int REQUEST_TAKE_PHOTO=10;
+
+    ImageView iv=null;
+    Button btn_takepic=null;
+    Button btn_choose=null;
+    Button btn_load=null;
+    EditText editText;
+    private Uri filepath;
+
+    DatabaseReference mRootRef=FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("name");
+    private String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "권한 설정 완료");
+            } else {
+                Log.d(TAG, "권한 설정 요청");
+                ActivityCompat.requestPermissions(Face.this, new String[]
+                        {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+        //사진
+        btn_takepic = (Button) findViewById(R.id.take_picture);
+        iv = (ImageView) findViewById(R.id.image);
+        editText=(EditText)findViewById(R.id.edit_name);
+        btn_takepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capture();
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText("");
+            }
+        });
+
+        //storage
+        btn_choose = (Button) findViewById(R.id.bt_choose);
+        btn_load = (Button) findViewById(R.id.bt_upload);
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
+            }
+        });
+
+        btn_load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
+            }
+        });
+        database=FirebaseDatabase.getInstance();
+
+        databaseReference=database.getReference("User");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Register",String.valueOf(error.toException()));
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permissson: " + permissions[0] + "was " + grantResults[0]);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //check the permission
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            iv.setImageBitmap(imageBitmap);
+        }
+
+        //take pic
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            File file = new File(mCurrentPhotoPath);
+            Bitmap bitmap;
+            if (Build.VERSION.SDK_INT >= 29) {
+                ImageDecoder.Source source =
+                        ImageDecoder.createSource(getContentResolver(), Uri.fromFile(file));
+                try {
+                    bitmap = ImageDecoder.decodeBitmap(source);
+                    if (bitmap != null) {
+                        iv.setImageBitmap(bitmap);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+                    if (bitmap != null) {
+                        iv.setImageBitmap(bitmap);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        //image choose
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            filepath = data.getData();
+            Log.d(TAG, "uri:" + String.valueOf(filepath));
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                iv.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HH").format(new Date());
+        String name="eunju"; String imageFileName= name + "_";
+        //String imageFileName="JPGE_"+timeStamp + "_";
+        File storageDir=getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File storageDir=new File(Environment.getExternalStorageDirectory()+"/eunju/",imageFileName);
+        if(!storageDir.exists())storageDir.mkdirs();
+        File image=File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath=image.getAbsolutePath();
+        return image;
+    }
+
+    public void capture(){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            File photoFile = null;
+
+            try{
+                photoFile = createImageFile();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+            if(photoFile!=null){
+                Uri photoURI= FileProvider.getUriForFile(this,
+                        "com.example.facerecognitionlock",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(takePictureIntent,REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void uploadFile(){
+        if(filepath!=null){
+            final ProgressDialog progressDialog=new ProgressDialog(this);
+            progressDialog.setTitle("UpLoading...");
+            progressDialog.show();
+
+            FirebaseStorage storage=FirebaseStorage.getInstance();
+
+            String name=editText.getText().toString();
+            conditionRef.setValue(name);
+            String filename=name;
+            StorageReference storageRef=storage.getReferenceFromUrl("gs://fir-connjava.appspot.com/").child("images/"+filename);
+
+            storageRef.putFile(filepath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Upload Success!",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Upload Fail!",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @SuppressWarnings("VisibleForTests")
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress=(100*snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded "+ ((int)progress)+"%...");
+                        }
+                    });
+        }else{
+            Toast.makeText(getApplicationContext(),"파일을 먼저 선택하세요.",Toast.LENGTH_SHORT).show();
+        }
+        editText.setText(" ");
+    }
+}
