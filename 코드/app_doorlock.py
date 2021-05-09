76729c2b5d7ef8b92c4964a8c2108b2baa753d2c
Bm941201 <77478050+Bm941201@users.noreply.github.com>
2021-05-09 22:52:30 +0900

    Add files via upload

---
+++ 코드/app_doorlock.py
@@ -0,0 +1,99 @@
+#import cv2
+
+ 
+
+#import numpy as np
+
+ 
+
+import os
+
+ 
+
+import time
+
+ 
+
+import RPi.GPIO as GPIO
+
+from datetime import datetime
+
+ 
+
+import firebase_admin
+
+from firebase_admin import credentials,db
+
+ 
+
+cred = credentials.Certificate("fir-connjava-firebase-adminsdk-qnktd-3592c424ff.json")
+
+default_app = firebase_admin.initialize_app(cred, {
+
+    'storageBucket': 'fir-connjava.appspot.com',
+
+    'databaseURL':'https://fir-connjava-default-rtdb.firebaseio.com/'
+
+})
+
+ 
+
+ref_door=db.reference('door')
+
+ref=db.reference()
+
+ # open_door()
+
+lock =11
+
+GPIO.setwarnings(False)
+
+ 
+
+GPIO.setmode(GPIO.BOARD)
+
+ 
+
+GPIO.setup(lock, GPIO.OUT)
+
+ 
+
+while True:
+
+    door=ref_door.get()
+
+    print(door)
+
+    if door=="open" or door=="close":
+
+        print(door)
+
+        GPIO.output(lock, GPIO.HIGH)
+
+        time.sleep(1)
+
+        GPIO.output(lock, GPIO.LOW)
+
+        time.sleep(1)
+
+        ref.update({'door':'wait'})
+
+'''
+
+    elif door=="close":
+
+	print(door)
+
+	GPIO.output(lock, GPIO.HIGH)
+
+	time.sleep(1)
+
+	GPIO.output(lock, GPIO.LOW)
+
+	time.sleep(3)
+
+	ref.update({'door':'wait'})
+
+'''
+
+GPIO.cleanup()
