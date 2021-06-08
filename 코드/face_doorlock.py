1bd0cc0b5181d81a602af839ea8e24d7f168e25f
Bm941201 <77478050+Bm941201@users.noreply.github.com>
2021-06-09 00:42:53 +0900

    Update face_doorlock.py

--- 코드/face_doorlock.py
+++ 코드/face_doorlock.py
@@ -6,50 +6,16 @@ import RPi.GPIO as GPIO
 
 from datetime import datetime
 
-import firebase_admin
+def main():
+    lock = 17
+    GPIO.setwarnings(False)
 
-from firebase_admin import credentials,db
+    GPIO.setmode(GPIO.BCM)
 
-cred = credentials.Certificate("fir-connjava-firebase-adminsdk-qnktd-8f492c5c5a.json")
+    GPIO.setup(lock, GPIO.OUT)
 
-default_app = firebase_admin.initialize_app(cred, {
-    'storageBucket': 'fir-connjava.appspot.com',
-
-    'databaseURL':'https://fir-connjava-default-rtdb.firebaseio.com/'
-})
-
-ref_door=db.reference('door')
-
-ref=db.reference()
-
-# open_door()
-
-lock =17
-
-GPIO.setwarnings(False)
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(lock, GPIO.OUT)
-
-while True:
-
-    door=ref_door.get()
-
-    if door=="open" or door=="close":
-
-        print('문이 열렸습니다.')
-
-        GPIO.output(lock, GPIO.HIGH)
-
-        time.sleep(1)
-
-        GPIO.output(lock, GPIO.LOW)
-
-        time.sleep(1)
-
-        ref.update({'door':'wait'})
-
-GPIO.cleanup()
-
-
+    print("문이 열렸습니다.")
+    GPIO.output(lock, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(lock,GPIO.LOW)
+    time.sleep(2)
